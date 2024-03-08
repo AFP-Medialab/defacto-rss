2ae9896fa29fdbd9e6485bdcf2a2d45a8db78ac9
Bertrand Goupil <bertrand.goupil@afp.com>
2024-03-08 15:44:06 +0100

    update build docx

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ echo "*** Producing HTML ***"
 asciidoctor Defacto_rss.adoc
 echo "*** Producing PDF ***"
 asciidoctor-pdf --theme defacto-theme.yml Defacto_rss.adoc
+echo "*** Producion DOCX ***"
+asciidoctor -b docbook  Defacto_rss.adoc
+pandoc -r docbook -t docx -o Defacto_rss.docx Defacto_rss.xml
